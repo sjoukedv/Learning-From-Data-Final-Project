cdcab793e6f6fa610681ec268fc28dfdd5626425
Sjouke de Vries <sjoukedevries@live.nl>
2021-10-19 13:34:31 +0200

    store results in json

--- BaseModel.py
+++ BaseModel.py
@@ -1,7 +1,37 @@
-import numpy as np
+from abc import ABC, abstractmethod
+import json, argparse, os
+
 class BaseModel(ABC):
     def __init__(self):
         self.name = "BaseModel"
+        self.args = self.create_arg_parser()
+
+    def create_arg_parser(self):
+        parser = argparse.ArgumentParser()
+
+        for argument in self.arguments:
+
+            if argument.get("type") != None:
+                parser.add_argument(
+                    argument["command"], 
+                    argument["refer"],
+                    default=argument.get("default"),
+                    type=argument.get("type"),
+                    help=argument.get("help"),
+                    action=argument.get("action")
+                )
+            else:
+                parser.add_argument(
+                    argument["command"], 
+                    argument["refer"],
+                    default=argument.get("default"),
+                    help=argument.get("help"),
+                    action=argument.get("action")
+                )
+        
+        args = parser.parse_args()
+        return args
+
 
     @abstractmethod
     def create_model(self):
@@ -32,5 +62,11 @@ class BaseModel(ABC):
         pass 
 
     def write_run_to_file(self, version, parameters, results):
-        # results/NaiveBayes/experiment_01.csv
-        np.savetxt('results/' + self.name + '/' + 'experiment_' + str(version).zfill(2) + '.csv', results, delimiter=",")
+        # make sure (sub)directory exists
+        os.makedirs('results/' + self.name, exist_ok=True)
+
+        # write results to file
+        json.dump({
+            'parameters' : parameters,
+            'results' : results
+            }, open('results/' + self.name + '/' + 'experiment_' + str(version).zfill(2) + '.json', 'w'))
