fe62d64088d9877fd94c0bf33253c43113b0013d
Sjouke de Vries <sjoukedevries@live.nl>
2021-10-27 12:28:59 +0200

    Bayes Final Model

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,6 +3,9 @@
 '''Naive bayes classifier '''
 
 import os
+# DEBUG
+# fixes cudart64_110.dll error
+# os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
 import json
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -116,7 +119,9 @@ if __name__ == "__main__":
     nb = NaiveBayes()
 
     X_train, Y_train, X_dev, Y_dev, X_test, Y_test = read_articles() 
-    X_train, Y_train = nb.under_sample_training_data(X_train, Y_train)
+
+    if nb.args.undersample:
+        X_train, Y_train = nb.under_sample_training_data(X_train, Y_train)
 
     nb.param_grid = {
         'cls__alpha': [1.0, 0.75, 0.5],
